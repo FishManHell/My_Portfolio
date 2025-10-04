334a298419a0362d6a5c2dad1e04261b1d4a92a1
Denys <leonard66693@gmail.com>
2025-10-05 01:25:49 +0300

    style changes

---
+++ src/entities/Projects/mocks/projectListResponsive.ts
@@ -0,0 +1,16 @@
+export const responsive = {
+    desktop: {
+        breakpoint: { max: 1920, min: 1161 },
+        items: 3,
+        slidesToSlide: 1
+    },
+    tablet: {
+        breakpoint: { max: 1160, min: 771 },
+        items: 2,
+        slidesToSlide: 1
+    },
+    mobile: {
+        breakpoint: { max: 770, min: 0 },
+        items: 1
+    }
+};
--- src/entities/Projects/ui/ProjectsList/ProjectsList.tsx
+++ src/entities/Projects/ui/ProjectsList/ProjectsList.tsx
@@ -6,7 +6,7 @@ import {useMemo} from "react";
 import {Project} from "../../module/types/project";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
-
+import {responsive} from "../../mocks/projectListResponsive";
 
 interface ProjectsListProps {
     className?: string;
@@ -21,55 +21,21 @@ export const ProjectsList = (props: ProjectsListProps) => {
         return projects?.map(project => <ProjectItem project={project} key={project.id}/>)
     }, [projects]);
 
-    // console.log(printProjectItems, "printProjectItems")
-    //
-    // const projectListRender = () => {
-    //     if (projectsLoading) {
-    //         return [1, 2, 3, 4].map(i => <ProjectItem loading key={i} />);
-    //     }
-    //
-    //     if (printProjectItems.length > 0) {
-    //         return printProjectItems;
-    //     }
-    //
-    //
-    //     return null
-    //
-    // };
-
-    const responsive = {
-        desktop: {
-            breakpoint: { max: 1920, min: 1161 },
-            items: 3,
-            slidesToSlide: 1
-        },
-        tablet: {
-            breakpoint: { max: 1160, min: 771 },
-            items: 2,
-            slidesToSlide: 1
-        },
-        mobile: {
-            breakpoint: { max: 770, min: 0 },
-            items: 1
-        }
-    };
-
 
     return (
         <section className={classNames(cls["projects-list"], className)}>
             <SectionHeader title="Projects" subtitle="Things I’ve built so far"/>
 
             <Carousel
-                arrows
-                responsive={responsive}
-                containerClass={cls["projects-list_carousel_container"]}
-                itemClass={cls["projects-list_carousel_container_item"]}
                 partialVisible
                 slidesToSlide={1}
                 infinite
                 keyBoardControl
                 swipeable
                 // removeArrowOnDeviceType={["mobile"]}
+                responsive={responsive}
+                containerClass={cls["projects-list_carousel_container"]}
+                itemClass={cls["projects-list_carousel_container_item"]}
             >
                 {projectsLoading && [1, 2, 3, 4].map(i => <ProjectItem loading key={i} />)}
                 {!projectsLoading && printProjectItems?.length && printProjectItems}
