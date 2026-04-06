843b17af044bc6e6bc4c18cc32af81102368c272
Denys <leonard66693@gmail.com>
2026-04-06 17:10:03 +0300

    add new logo icons

--- src/widgets/TechStack/model/mock/tech_stacks.ts
+++ src/widgets/TechStack/model/mock/tech_stacks.ts
@@ -8,7 +8,23 @@ import ReduxIcon from "shared/assets/icons/Redux.svg";
 import GitIcon from "shared/assets/icons/Git.svg";
 import MobXIcon from "shared/assets/icons/Mobx.svg";
 import VueIcon from "shared/assets/icons/Vue.svg";
-
+import AntDesignIcon from "shared/assets/icons/AntDesign.svg";
+import AxiosIcon from "shared/assets/icons/Axios.svg";
+import FigmaIcon from "shared/assets/icons/Figma.svg";
+import GoogleIcon from "shared/assets/icons/Google.svg";
+import MongoDbIcon from "shared/assets/icons/MongoDb.svg";
+import MuiIcon from "shared/assets/icons/Mui.svg";
+import NextJsIcon from "shared/assets/icons/NextJs.svg";
+import NodeJsIcon from "shared/assets/icons/NodeJs.svg";
+import PiniaIcon from "shared/assets/icons/Pinia.svg";
+import PostgresSqlIcon from "shared/assets/icons/PostgresSql.svg";
+import PrimeVueIcon from "shared/assets/icons/PrimeVue.svg";
+import VercelIcon from "shared/assets/icons/Vercel.svg";
+import ViteIcon from "shared/assets/icons/ViteJs.svg";
+import GraphQlIcon from "shared/assets/icons/GraphQl.svg";
+import StorybookIcon from "shared/assets/icons/Storybook.svg";
+import TailwindIcon from "shared/assets/icons/Tailwind.svg";
+import WebpackIcon from "shared/assets/icons/Webpack.svg";
 
 export const techStacksIconCollection = {
     html: HtmlIcon,
@@ -18,7 +34,24 @@ export const techStacksIconCollection = {
     ts: TSIcon,
     react: ReactIcon,
     redux: ReduxIcon,
-    git: GitIcon,
     mobx: MobXIcon,
-    vue: VueIcon
+    vue: VueIcon,
+    pinia: PiniaIcon,
+    nextjs: NextJsIcon,
+    nodejs: NodeJsIcon,
+    antd: AntDesignIcon,
+    mui: MuiIcon,
+    primevue: PrimeVueIcon,
+    axios: AxiosIcon,
+    mongodb: MongoDbIcon,
+    postgresql: PostgresSqlIcon,
+    graphql: GraphQlIcon,
+    tailwind: TailwindIcon,
+    webpack: WebpackIcon,
+    storybook: StorybookIcon,
+    figma: FigmaIcon,
+    google: GoogleIcon,
+    vercel: VercelIcon,
+    vite: ViteIcon,
+    git: GitIcon,
 } as const;
